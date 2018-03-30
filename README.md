4af8ad9c412d4b3d2f8089ed5ca82c02f027b14a
Mateusz Sawka <m.e.sawka@gmail.com>
2018-03-30 18:00:22 +0200

    initialize nalik2000

--- README.md
+++ README.md
@@ -1,27 +1,5 @@
-# Drum machine
+Forked from [GitHub](https://github.com/maryrosecook/drum-machine)
 
-How to write a simple drum machine that runs in the browser.
+nalik2000βετα drum machine
 
-![Screenshot of the drum machine](screenshot.gif)
-
-Running version at http://drum-machine.maryrosecook.com
-
-## Video of Mary livecoding the drum machine
-
-https://www.youtube.com/watch?v=jB219tQWjEg
-
-## Annotated source code
-
-The <a href="/drum-machine.js">heavily annotated source code</a> is 114 lines long.
-
-## Getting the code
-
-Clone it from [GitHub](https://github.com/maryrosecook/drum-machine)
-
-```bash
-$ git clone git@github.com:maryrosecook/drum-machine.git
-```
-
-## Contact
-
-Mary Rose Cook - http://maryrosecook.com
+[DEMO](https://nalik2000.surge.sh)
--- drum-machine.js
+++ drum-machine.js
@@ -1,6 +1,205 @@
+(function (){
+  window.moog = {
+    steps:  16,
+    keys: {},
+    currentButton: {}
+  }
+  initAudioContext()
+  setupWindowEvents()
+})()
+
+function initAudioContext() {
+  try {
+    var contextClass = (window.AudioContext ||
+      window.webkitAudioContext ||
+      window.mozAudioContext ||
+      window.oAudioContext ||
+      window.msAudioContext);
+    if (!contextClass) {
+      var node = document.createElement("h1")
+      var textnode = document.createTextNode("It won't work... ;(")
+      node.appendChild(textnode)
+      document.getElementsByTagName('body')[0].appendChild(node)
+    }
+    context = new contextClass();
+  }
+  catch (e) {
+    alert('Web Audio API is not supported in this browser');
+    var node = document.createElement("h1")
+    var textnode = document.createTextNode("It won't work... ;(")
+    node.appendChild(textnode)
+    document.getElementsByTagName('body')[0].appendChild(node)
+  }
+
+  if (context.state === 'suspended') {
+    var resume = function () {
+      context.resume();
+
+      setTimeout(function () {
+        if (context.state === 'running') {
+          document.body.removeEventListener('touchend', resume, false);
+        }
+      }, 0);
+    };
+
+    document.body.addEventListener('touchend', resume, false);
+  }
+}
+
+function setupWindowEvents() {
+  setCanvasWidth()
+  setupButtonClicking()
+  window.onresize = setCanvasWidth
+  window.onkeyup = function (e) {
+    window.moog.keys[e.keyCode] = false;
+  }
+  window.onkeydown = function (e) {
+    window.moog.keys[e.keyCode] = true;
+  }
+}
+
+function setCanvasWidth() {
+  var padding = 10
+  var margin = 10
+  var canvas = document.getElementById("screen")
+  var maxWidth = window.innerWidth - 15 - margin * 2 - padding
+  var maxHeight = window.innerHeight
+  window.moog.BUTTON_SIZE = Math.min(maxWidth / (window.moog.steps * 2), maxHeight / 12 );
+  canvas.width = window.innerWidth - 15 - margin * 2 - padding
+  canvas.height = window.moog.BUTTON_SIZE * 10.5
+  document.getElementsByClassName('moog')[0].style.width = canvas.width + 20 + 'px'
+}
+
+
+// Handle events
+
+function setupButtonClicking() {
+  var canvas = document.getElementById("screen")
+  canvas.addEventListener("mousemove", padListener);
+  canvas.addEventListener("click", padClick);
+  canvas.addEventListener("touchend", handleTouchEnd, false);
+};
+
+function padListener(e) {
+  var p = {x: e.offsetX, y: e.offsetY};
+  var cursorOnButton = null
+
+  data.tracks.forEach(function (track, row) {
+    track.steps.forEach(function (on, column) {
+      if (isPointInButton(p, column, row)) {
+        if (shouldToggle(column, row)) {
+          track.steps[column] = !on;
+          window.moog.currentButton = {column, row}
+        }
+        cursorOnButton = true
+      }
+    });
+  });
+  window.lastMoveWithinButton = cursorOnButton
+  if (!cursorOnButton) {
+    window.moog.currentButton = {}
+  }
+}
+
+function padClick(e) {
+  var p = {x: e.offsetX, y: e.offsetY};
+  data.tracks.forEach(function (track, row) {
+    track.steps.forEach(function (on, column) {
+      if (isPointInButton(p, column, row)) {
+        track.steps[column] = !on;
+        window.moog.currentButton = {column, row}
+      }
+    });
+  });
+  window.lastMoveWithinButton = true
+};
+
+function handleTouchEnd (e) {
+  var touches = e.changedTouches;
+  var mockedClick = {
+    offsetX: touches[0].pageX,
+    offsetY: touches[0].pageY
+  }
+  padListener(mockedClick);
+}
+
+
+// HELPERS
+
+function shouldToggle(column, row) {
+  var moog = window.moog
+  if (!moog.keys[16]) {
+    return
+  }
+  if (window.lastMoveWithinButton) {
+    return false
+  }
+  if (moog.currentButton.column === column && moog.currentButton.row === row) {
+    return false
+  }
+  if (!window.lastMoveWithinButton) {
+    return true
+  }
+}
+
+
+// CONTROL BUTTONS
+
+function initTracks(numberOfSteps) {
+  window.moog.steps = numberOfSteps
+  data.tracks = prepareTracks(data.tracks)
+  setCanvasWidth()
+}
+
+function clearPads() {
+  data.tracks.forEach(function (track, row) {
+      track.steps.forEach(function (on, column) {
+        track.steps[column] = false;
+      })
+    }
+  );
+}
+
+function generate() {
+  var baseProbability = 0.07
+  var lastNoteMultiplier = 2
+  var kickMultiplier = 2
+  data.tracks.forEach(function (track, row) {
+      track.steps.forEach(function (on, column) {
+        var isLastNote = ((column + 1)%4 === 0)
+        var isKick = row == 5
+        var treshold = baseProbability
+        if (isKick) {
+          treshold = treshold * kickMultiplier
+        }
+        if (isLastNote) {
+          treshold = treshold * lastNoteMultiplier
+        }
+        track.steps[column] = Math.random() > 1-treshold;
+      })
+    }
+  );
+}
+
+function invert() {
+  data.tracks.forEach(function (track, row) {
+      track.steps.forEach(function (on, column) {
+        track.steps[column] = !track.steps[column];
+      })
+    }
+  );
+}
+
+
+///////////////////////////////
+//
+//  ORIGINAL CODE (mostly)
+//   tracks creation, canvas
+//
+//////////////////////////////
 // Create the object that contains functions that use web audio to
 // make sound.
-var audio = new AudioContext();
+var audio = context;
 
 // Create the data for the drum machine.
 var data = {
@@ -10,19 +209,23 @@ var data = {
 
   // `tracks` holds the six tracks of the drum machine.  Each track
   // has a sound and sixteen steps (or beats).
-  tracks: [createTrack("gold", note(audio, 880)),
-           createTrack("gold", note(audio, 659)),
-           createTrack("gold", note(audio, 587)),
-           createTrack("gold", note(audio, 523)),
-           createTrack("gold", note(audio, 440)),
-           createTrack("dodgerblue", kick(audio))]
+  tracks: prepareTracks()
 };
 
+function prepareTracks(input) {
+  var data = input || []
+  return [createTrack("gold", note(audio, 880), data[0]),
+    createTrack("gold", note(audio, 659), data[1]),
+    createTrack("gold", note(audio, 587), data[2]),
+    createTrack("gold", note(audio, 523), data[3]),
+    createTrack("gold", note(audio, 440), data[4]),
+    createTrack("dodgerblue", kick(audio), data[5])]
+}
 // Update
 // ------
 
 // Runs every hundred milliseconds.
-setInterval(function() {
+setInterval(function () {
 
   // Increase `data.step` by one.  If `data.step` is `15` (the last
   // step) loop back around to `0` (the first step).
@@ -31,8 +234,12 @@ setInterval(function() {
   // Find all the tracks where the current step is on.  Play the
   // sounds for those tracks.
   data.tracks
-    .filter(function(track) { return track.steps[data.step]; })
-    .forEach(function(track) { track.playSound(); });
+    .filter(function (track) {
+      return track.steps[data.step];
+    })
+    .forEach(function (track) {
+      track.playSound();
+    });
 }, 100);
 
 // Draw
@@ -42,16 +249,32 @@ setInterval(function() {
 // in the canvas element.
 var screen = document.getElementById("screen").getContext("2d");
 
+
 // **draw()** draws the drum machine.  Called once at the beginning of
 // the program.  It's then called 60 times a second forever (see the
 // call to `requestAnimationFrame()` below).
 (function draw() {
+  // screen && screen.scale(2, 2)
+  function drawIntervals(x) {
+    for (var i = 1; i < x; i++) {
+      screen.beginPath();
+      screen.moveTo(window.moog.BUTTON_SIZE * 8 * i - 1, 0);
+      screen.lineTo(window.moog.BUTTON_SIZE * 8 * i - 1, window.moog.BUTTON_SIZE * 9.5);
+      screen.stroke();
+    }
+    screen.beginPath();
+    screen.moveTo(0, window.moog.BUTTON_SIZE * 9.5);
+    screen.lineTo(window.moog.BUTTON_SIZE * 8 * i, window.moog.BUTTON_SIZE * 9.5);
+    screen.stroke();
+  }
 
   // Clear away the previous drawing.
   screen.clearRect(0, 0, screen.canvas.width, screen.canvas.height);
 
   // Draw all the tracks.
   drawTracks(screen, data);
+
+  drawIntervals(window.moog.steps/4)
 
   // Draw the pink square that indicates the current step (beat).
   drawButton(screen, data.step, data.tracks.length, "deeppink");
@@ -60,40 +283,10 @@ var screen = document.getElementById("screen").getContext("2d");
   requestAnimationFrame(draw);
 })();
 
-// Handle events
-// -------------
-
-// **setupButtonClicking()** sets up the event handler that will make
-// mouse clicks turn track buttons on and off.
-(function setupButtonClicking() {
-
-  // Every time the user clicks on the canvas...
-  document.getElementById("screen").addEventListener("click", function(e) {
-
-    // ...Get the coordinates of the mouse pointer relative to the
-    // canvas...
-    var p = { x: e.offsetX, y: e.offsetY };
-
-    // ...Go through every track...
-    data.tracks.forEach(function(track, row) {
-
-      // ...Go through every button in this track...
-      track.steps.forEach(function(on, column) {
-
-        // ...If the mouse pointer was inside this button...
-        if (isPointInButton(p, column, row)) {
-
-          // ...Switch it off if it was on or on if it was off.
-          track.steps[column] = !on;
-        }
-      });
-    });
-  });
-})();
 
 // **note()** plays a note with a pitch of `frequency` for `1` second.
 function note(audio, frequency) {
-  return function() {
+  return function () {
     var duration = 1;
 
     // Create the basic note as a sine wave.  A sine wave produces a
@@ -125,7 +318,7 @@ function note(audio, frequency) {
 
 // **kick()** plays a kick drum sound for `1` second.
 function kick(audio) {
-  return function() {
+  return function () {
     var duration = 2;
 
     // Create the basic note as a sine wave.  A sine wave produces a
@@ -148,7 +341,7 @@ function kick(audio) {
       // An amplifier reduces the volume of the tone from 40% to 0
       // over the duration of the tone.  This produces an echoey
       // effect.
-      createAmplifier(audio, 0.4, duration),
+      createAmplifier(audio, 1, duration),
 
       // The amplified output is sent to the browser to be played
       // aloud.
@@ -210,23 +403,33 @@ function chain(soundNodes) {
 // (`true`) or off (`false`).  It contains `color`, the color to draw
 // buttons when they are on.  It contains `playSound`, the function
 // that plays the sound of the track.
-function createTrack(color, playSound) {
-  var steps = [];
-  for (var i = 0; i < 16; i++) {
-    steps.push(false);
+function createTrack(color, playSound, data) {
+  var stepCount = window.moog.steps
+  var prevStepCount = data && data.steps && data.steps.length
+  var newSteps = data && data.steps
+
+  if (  stepCount > prevStepCount ) {
+    for (i = 1; i < stepCount/prevStepCount; i++) {
+      newSteps = newSteps.concat(data.steps)
+    }
+    newSteps = newSteps.concat(newSteps)
   }
 
-  return { steps: steps, color: color, playSound: playSound };
-};
+  var steps = [];
+  for (var i = 0; i < stepCount; i++) {
+    var on = data && data.steps && newSteps[i]
+    steps.push(on);
+  }
 
-var BUTTON_SIZE = 26;
+  return {steps: steps, color: color, playSound: playSound};
+};
 
 // **buttonPosition()** returns the pixel coordinates of the button at
 // `column` and `row`.
 function buttonPosition(column, row) {
   return {
-    x: BUTTON_SIZE / 2 + column * BUTTON_SIZE * 1.5,
-    y: BUTTON_SIZE / 2 + row * BUTTON_SIZE * 1.5
+    x: window.moog.BUTTON_SIZE / 2 + column * window.moog.BUTTON_SIZE * 2,
+    y: window.moog.BUTTON_SIZE / 2 + row * window.moog.BUTTON_SIZE * 1.5
   };
 };
 
@@ -234,17 +437,17 @@ function buttonPosition(column, row) {
 function drawButton(screen, column, row, color) {
   var position = buttonPosition(column, row);
   screen.fillStyle = color;
-  screen.fillRect(position.x, position.y, BUTTON_SIZE, BUTTON_SIZE);
+  screen.fillRect(position.x, position.y, window.moog.BUTTON_SIZE, window.moog.BUTTON_SIZE);
 };
 
 // **drawTracks()** draws the tracks in the drum machine.
 function drawTracks(screen, data) {
-  data.tracks.forEach(function(track, row) {
-    track.steps.forEach(function(on, column) {
+  data.tracks.forEach(function (track, row) {
+    track.steps.forEach(function (on, column) {
       drawButton(screen,
-                 column,
-                 row,
-                 on ? track.color : "lightgray");
+        column,
+        row,
+        on ? track.color : "lightgray");
     });
   });
 };
@@ -254,7 +457,7 @@ function drawTracks(screen, data) {
 function isPointInButton(p, column, row) {
   var b = buttonPosition(column, row);
   return !(p.x < b.x ||
-           p.y < b.y ||
-           p.x > b.x + BUTTON_SIZE ||
-           p.y > b.y + BUTTON_SIZE);
+    p.y < b.y ||
+    p.x > b.x + window.moog.BUTTON_SIZE ||
+    p.y > b.y + window.moog.BUTTON_SIZE);
 };
